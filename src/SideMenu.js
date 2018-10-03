39e62d0bd9a7e6b2af5b1c643afe4677c9e68f18
Christopher ADAMS <CJ.ADAMS@outlook.com>
2018-10-03 18:15:25 +0200

    Premier commit du  layout

---
+++ src/SideMenu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SideMenu = () => (
+  <div id="SideMenu" className="col-md-3">
+    <div>
+      <ul>
+        <li>Desktop menu placeholder</li>
+      </ul>
+    </div>
+  </div>
+);
+
+export default SideMenu;
