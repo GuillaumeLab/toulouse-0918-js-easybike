c7450d6d4471df12fb7103a27cfb1c07b56d607d
Bruno Belorgey <bruno.belorgey@laposte.net>
2018-10-12 14:17:34 +0200

    correction petit bug

--- src/components/MapLeaflet.js
+++ src/components/MapLeaflet.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
-import { Map, TileLayer } from 'react-leaflet';
-import { Marker, Popup } from 'react-leaflet';
+import { Map, TileLayer,  Marker, Popup } from 'react-leaflet';
 
 import MarkersLayer from './MarkersLayer';
 import Geolocation from 'react-geolocation';
@@ -10,15 +9,15 @@ import Geolocation from 'react-geolocation';
 
 const usersMarkerDefault = {
   center: [43.599927, 1.443197],
-  zoom: 20
-};
-
-const mapConfig = {
-  center: [43.6036786, 1.4328012],
   zoom: 15
 };
 
-class MapLeaflet extends Component {43.6036786
+// const mapConfig = {
+//   center: [43.6036786, 1.4328012],
+//   zoom: 15
+// };
+
+class MapLeaflet extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -44,7 +43,7 @@ class MapLeaflet extends Component {43.6036786
                   longitude = defaultLongUser;
                 }
               return (
-                  <Map center={[latitude,longitude]} zoom={mapConfig.zoom} className="map__reactleaflet">
+                  <Map center={[latitude,longitude]} zoom={usersMarkerDefault.zoom} className="map__reactleaflet">
                     <TileLayer
                       url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
                       attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
