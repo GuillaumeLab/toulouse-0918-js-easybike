1257083b24003a46cced9939d6696df0be291df6
Bruno Belorgey <bruno.belorgey@laposte.net>
2018-10-17 15:10:16 +0200

    ajout du filter des stations

--- src/components/MarkersLayer.js
+++ src/components/MarkersLayer.js
@@ -59,10 +59,14 @@ class MarkersLayer extends Component {
 
   render() {
     const { stationsList, error } = this.state;
-
+    // const stationsToDisplay = 'all';
     const maxWidth = 400;
     const minWidth = 340;
-    const leafletMarkers = stationsList.map(stationData => (
+    // const leafletMarkers = stationsList.map(stationData => (
+    const leafletMarkers = stationsList.filter(stationData => (stationData.available_bikes !== 0 && stationsToDisplay === "bikes") || 
+                                                              (stationData.available_bike_stands !== 0 && stationsToDisplay === "freeSpaces") ||
+                                                               stationsToDisplay === "all")
+        .map(stationData => (
       <Marker
         icon={L.divIcon({
           className: 'custom-icon',
