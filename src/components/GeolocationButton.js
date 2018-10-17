33c688075bd2b24e076c837af45d93f3504415ec
Geoffroy Periot <geoffroy.periot@gmail.com>
2018-10-17 11:15:30 +0200

    ajout du state stationsToDisplay pour la gestion des stations à afficher

--- src/components/GeolocationButton.js
+++ src/components/GeolocationButton.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import Control from 'react-leaflet-control';
 import { geolocIcon } from '../images/geoloc-icon.svg';
 
 
 const GeolocationButton = () => (
   <div>
     <button type="button">
-      Reset View
+      <img src={geolocIcon} alt="target icon" />
     </button>
   </div>
 );
+
 export default GeolocationButton;
