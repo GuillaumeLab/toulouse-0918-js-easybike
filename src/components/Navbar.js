78dbf4462297d07996dec68253fcb3188e3ed0ca
Christopher ADAMS <CJ.ADAMS@outlook.com>
2018-10-18 10:11:01 +0200

    Added link to intinerary on Google Maps

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,22 @@ const Navbar = () => (
     <div className="col-2">
       <a className="navbar-brand" href="index.html"><h1>EasyBike</h1></a>
     </div>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-      aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
     </button>
     <div id="navbarNavAltMarkup" className="col-md-8 collapse navbar-collapse">
       <div className="navbar-nav text-right text-md-center mx-auto">
         <a className="nav-item nav-link" href="#">Prendre un vélo</a>
         <a className="nav-item nav-link" href="#">Déposer un vélo</a>
-        <a className="nav-item nav-link" href="#">Calcul d'intinéraire</a>
+        <a className="nav-item nav-link" href="https://www.google.fr/maps/dir/" target="_blank" rel="noopener noreferrer">Calcul d&apos;itinéraire</a>
         <a className="nav-item nav-link" href="#">Filtrer</a>
         <a className="nav-item nav-link" href="#">Favoris</a>
       </div>
