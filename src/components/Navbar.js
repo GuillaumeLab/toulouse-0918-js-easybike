361d8b1e44b6911199a1c34f93022a2235516ca0
Christopher ADAMS <CJ.ADAMS@outlook.com>
2018-10-19 14:45:04 +0200

    Added unsaved files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-<<<<<<< HEAD
 const getItinararyUrl = (str1, str2) => {
   console.log(str1, str2);
   const generateUrl = (`https://www.google.com/maps/dir/?api=1&origin=${str1}+toulouse&destination=${str2}toulouse&travelmode=bicycling`);
@@ -67,31 +66,4 @@ class Navbar extends Component {
   }
 }
 
-=======
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark">
-        <div className="col-2">
-          <a className="navbar-brand" href="index.html"><h1>EasyBike</h1></a>
-        </div>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div id="navbarNavAltMarkup" className="col-md-8 collapse navbar-collapse">
-          <div className="navbar-nav text-right text-md-center mx-auto">
-            <a className="nav-item nav-link" href="#">Prendre un vélo</a>
-            <a className="nav-item nav-link" href="#">Déposer un vélo</a>
-            <a className="nav-item nav-link" href="#">Calcul d&apos;itinéraire</a>
-            <a className="nav-item nav-link" href="#">Filtrer</a>
-            <a className="nav-item nav-link" href="#">Favoris</a>
-          </div>
-        </div>
-      </nav>
-    );
-  }
-}
-
->>>>>>> dev
 export default Navbar;
--- src/components/SvgStationIconGauge.js
+++ src/components/SvgStationIconGauge.js
@@ -7,12 +7,7 @@ class SvgStationIconGauge extends Component {
       <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 50 120" className="icon-station">
         <title>Station marker</title>
         <desc>Marker with gauge to display availability in bike stations</desc>
-<<<<<<< HEAD
-
-        <polygon points="2 2 48 2 48 80 25 118 2 80" stroke='#007BFF' strokeWidth="6" fill='#007BFF' />
-=======
         <polygon points="2 2 48 2 48 80 25 118 2 80" stroke="#333333" strokeWidth="4" fill="#007BFF" />
->>>>>>> dev
         <clipPath id="fill-icon">
           <polygon points="4 4 46 4 46 80 25 116 4 80 " strokeWidth="1" />
         </clipPath>
