b894e02297a246ac21f1829b4edcabb19c40173f
Benoît Hubert <benoithubert@gmail.com>
2018-10-26 11:21:40 +0200

    Fusion des modifications dans la Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,48 +1,34 @@
 import React, { Component } from 'react';
 
-const getItineraryUrl = (userPosition, closestStation) => 
-  {
-    console.log("userPosition", userPosition);
-    console.log("getItinary", closestStation);
-    return `https://www.google.com/maps/dir/?api=1&origin=${userPosition}&destination=${closestStation}`;
-  };
+const getItineraryUrl = (userPosition, closestStation) => {
+  return `https://www.google.com/maps/dir/?api=1&origin=${userPosition}&destination=${closestStation}`;
+};
 
 class Navbar extends Component {
   render() {
     const { getClosestStationPosition, userPosition } = this.props;
-    console.log("Navbar ",getClosestStationPosition("bikes"));
     return (
-      <nav className="navbar navbar-expand-lg navbar-light">
-        <div className="col-2">
-          <a className="navbar-brand" href="index.html"><h1>EasyBike</h1></a>
-        </div>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div id="navbarNavAltMarkup" className="col-md-8 collapse navbar-collapse">
-          <div className="navbar-nav text-right text-md-center mx-auto">
-            <a 
-              className="nav-item nav-link" 
-              href={getItineraryUrl(userPosition, getClosestStationPosition("bikes"))}
-              target="_blank"
-            >
-              Vite! Un vélo!
-            </a>
-            <a 
-              className="nav-item nav-link" 
-              href={getItineraryUrl(userPosition, getClosestStationPosition("freeSpaces"))}
-              target="_blank"
-            >
-            Vite! Poser mon vélo!</a>
-          </div>
+      <nav className="navbar">
+        <a className="navbar-brand" href="index.html"><h1>EasyBike</h1></a>
+        <div className="mx-auto">
+          <a
+            className="nav-item btn vite-button"
+            href={getItineraryUrl(userPosition, getClosestStationPosition("bikes"))}
+            target="_blank"
+          >
+            Vite
+            <br />
+            un vélo&nbsp;!
+          </a>
+          <a
+            className="nav-item btn vite-button"
+            href={getItineraryUrl(userPosition, getClosestStationPosition("freeSpaces"))}
+            target="_blank"
+          >
+            Vite
+            <br />
+            une place&nbsp;!
+          </a>
         </div>
       </nav>
     );
